1c52268c5b98a0688cf4ca14ce3d8eb5c2381220
Kateryna Ostrynska <kateriinag@gmail.com>
2022-09-16 15:45:21 +0300

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+const itemsMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img
+            loading = "lazy"
+            class="gallery__image"
+            src="${preview}"
+            alt="${description}"
+        />
+        </a>`;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery .gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
